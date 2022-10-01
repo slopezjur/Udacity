f6a7e6a3bdb7844350ce441f2eaf69f0cda8646d
Sergio López <slopezjur@users.noreply.github.com>
2022-10-01 19:28:04 +0200

    add a notification for the download

--- nd940-c3-advanced-android-programming-project-starter-master/starter/app/src/main/java/com/udacity/MainActivity.kt
+++ nd940-c3-advanced-android-programming-project-starter-master/starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
@@ -8,12 +9,14 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -35,6 +38,17 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        setRadioButtonSelector()
+
+        notificationManager = ContextCompat.getSystemService(
+            baseContext,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        createChannel()
+    }
+
+    private fun setRadioButtonSelector() {
         custom_button.setOnClickListener {
             if (current_url.isNotEmpty()) {
                 download()
@@ -71,13 +85,14 @@ class MainActivity : AppCompatActivity() {
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-            val id2 = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+
+            context?.let { sendNotification() }
         }
     }
 
     private fun download() {
         val request =
-            DownloadManager.Request(Uri.parse(UDACITY_URL))
+            DownloadManager.Request(Uri.parse(current_url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -89,6 +104,50 @@ class MainActivity : AppCompatActivity() {
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
+    private fun createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                getString(R.string.notification_channel),
+                NotificationManager.IMPORTANCE_LOW
+            ).apply { setShowBadge(false) }
+
+            notificationChannel.enableLights(true)
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = CHANNEL_ID
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun sendNotification() {
+
+        val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+
+        pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        action = NotificationCompat.Action(
+            R.drawable.ic_assistant_black_24dp,
+            applicationContext.getString(R.string.notification_button),
+            pendingIntent
+        )
+
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        ).setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setContentTitle(applicationContext.getString(R.string.notification_title))
+            .setContentText((applicationContext.getString(R.string.notification_description)))
+            .setContentIntent(pendingIntent)
+            .addAction(action)
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build())
+    }
+
     companion object {
         private const val GLIDE_URL =
             "https://github.com/bumptech/glide/archive/master.zip"
@@ -97,5 +156,6 @@ class MainActivity : AppCompatActivity() {
         private const val RETROFIT_URL =
             "https://github.com/square/retrofit/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
+        private const val NOTIFICATION_ID = 0
     }
 }
