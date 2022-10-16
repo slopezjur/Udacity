a8dd8f09ea8623c88796b8bebfcfbc8365cabe63
Sergio López <slopezjur@users.noreply.github.com>
2022-10-16 20:16:51 +0200

    update libraries

--- nd940-android-kotlin-c4-starter-master/starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ nd940-android-kotlin-c4-starter-master/starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,31 +1,40 @@
 package com.udacity.project4.locationreminders
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import com.udacity.project4.R
-import kotlinx.android.synthetic.main.activity_reminders.*
+import com.udacity.project4.databinding.ActivityRemindersBinding
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
 
+    private var _binding: ActivityRemindersBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders)
-
+        _binding = ActivityRemindersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                (nav_host_fragment as NavHostFragment).navController.popBackStack()
+                // TODO : fix
+                //(binding.nav_host_fragment as NavHostFragment).navController.popBackStack()
                 return true
             }
         }
